bd218be26aac59a24cd1ef21c792f10d2f9f94fc
unknown <tberry6@gmail.com>
2018-04-09 10:25:59 -0700

    Added letter constructor

---
+++ Letter.js
@@ -0,0 +1,11 @@
+function Letter(l) {
+    this.letter = l;
+    this.guessed = false;
+    this.display = function() {
+        if(this.guessed) return this.letter;
+        else return "_";
+    }
+    this.check = function(c) {
+        if(c === this.letter) this.guessed = true;
+    }
+}
