c47574090b10a95a7c02d6648cff346da386ce5d
unknown <tberry6@gmail.com>
2018-04-10 10:44:01 -0700

    Refactored methods to use prototype

--- Letter.js
+++ Letter.js
@@ -1,18 +1,20 @@
 function Letter(l) {
     this.letter = l;
     this.guessed = false;
-    this.toString = function () {
-        if (this.guessed) return this.letter;
-        else return "_";
+}
+
+Letter.prototype.toString = function () {
+    if (this.guessed) return this.letter;
+    else return "_";
+}
+
+Letter.prototype.check = function (c) {
+    var correct = false;
+    if (c.toLowerCase() === this.letter.toLowerCase()) {
+        if (!this.guessed) correct = true;
+        this.guessed = true;
     }
-    this.check = function (c) {
-        var correct = false;
-        if (c.toLowerCase() === this.letter.toLowerCase()) {
-            if(!this.guessed) correct = true;
-            this.guessed = true;
-        }
-        return correct;
-    }
+    return correct;
 }
 
 module.exports = Letter;
--- Word.js
+++ Word.js
@@ -7,32 +7,32 @@ function Word(word) {
     }
 
     this.guesses = [];
+}
 
-    this.toString = function () {
-        var string = "";
-        for (var i = 0; i < this.letters.length; i++) {
-            string = string.concat(" ", this.letters[i]);
-        }
-        return string;
+Word.prototype.toString = function () {
+    var string = "";
+    for (var i = 0; i < this.letters.length; i++) {
+        string = string.concat(" ", this.letters[i]);
     }
+    return string;
+}
 
-    this.check = function (c) {
-        var correct = false;
-        for (var i = 0; i < this.letters.length; i++) {
-            var bool = this.letters[i].check(c);
-            correct = correct || bool;
-        }
-        this.guesses.push(c.toLowerCase());
-        return correct;
+Word.prototype.check = function (c) {
+    var correct = false;
+    for (var i = 0; i < this.letters.length; i++) {
+        var bool = this.letters[i].check(c);
+        correct = correct || bool;
     }
+    this.guesses.push(c.toLowerCase());
+    return correct;
+}
 
-    this.complete = function () {
-        var complete = true;
-        for (var i = 0; i < this.letters.length; i++) {
-            if(!this.letters[i].guessed) complete = false;
-        }
-        return complete;
+Word.prototype.complete = function () {
+    var complete = true;
+    for (var i = 0; i < this.letters.length; i++) {
+        if (!this.letters[i].guessed) complete = false;
     }
+    return complete;
 }
 
 module.exports = Word;
